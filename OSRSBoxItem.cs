b686eb8aaf45af5cb5e18ea378efcc154f06fbdf
Twinki <thetwinkehh@gmail.com>
2021-04-06 05:22:06 -0500

    Squashed 'OSItemIndex.Models/' content from commit 3c4c96b
    git-subtree-dir: OSItemIndex.Models
    git-subtree-split: 3c4c96b2299c0cd3ee2b18467d24d13e9fdb0328

---
+++ OSRSBoxItem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace OSItemIndex.Models
+{
+    public class OsrsBoxItem : ItemEntity
+    {
+        /// <summary>
+        /// If the item is a duplicate.
+        /// </summary>
+        [Required]
+        [JsonPropertyName("duplicate")]
+        public bool? Duplicate { get; set; }
+
+        /// <summary>
+        /// If the item is noted.
+        /// </summary>
+        [Required]
+        [JsonPropertyName("noted")]
+        public bool? Noted { get; set; }
+
+        /// <summary>
+        /// If the item is a placeholder.
+        /// </summary>
+        [Required]
+        [JsonPropertyName("placeholder")]
+        public bool? Placeholder { get; set; }
+
+        /// <summary>
+        /// If the item is stackable (in inventory).
+        /// </summary>
+        [Required]
+        [JsonPropertyName("stackable")]
+        public bool? Stackable { get; set; }
+
+        /// <summary>
+        /// If the item is tradeable (only on GE).
+        /// </summary>
+        [Required]
+        [JsonPropertyName("tradeable_on_ge")]
+        public bool? TradeableOnGe { get; set; }
+
+        /// <summary>
+        /// The last time (UTC) the item was updated (in ISO8601 date format).
+        /// </summary>
+        [Required]
+        [JsonPropertyName("last_updated")]
+        public DateTime? LastUpdated { get; set; }
+
+        /// <summary>
+        /// OSRSBox item document.
+        /// </summary>
+        [Required]
+        [Column(TypeName = "json")]
+        [JsonPropertyName("document")]
+        public string Document { get; set; }
+    }
+}
