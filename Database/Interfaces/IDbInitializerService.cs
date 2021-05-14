d9b887cbd83ea05ad496266a72ab1de83ab94e3d
Twinki <thetwinkehh@gmail.com>
2021-05-14 16:18:00 -0400

    wip: introduce db initializer service

--- Database/Interfaces/IDbInitializerService.cs
+++ Database/Interfaces/IDbInitializerService.cs
@@ -4,9 +4,9 @@ using System.Threading.Tasks;
 
 namespace OSItemIndex.Data.Database
 {
-    public interface IDatabaseInitializerService
+    public interface IDbInitializerService
     {
         public void InitializeDatabase(IServiceProvider serviceProvider);
-        public Task InitializeDatabaseAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken);
+        public Task InitializeDatabaseAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken = default);
     }
 }
